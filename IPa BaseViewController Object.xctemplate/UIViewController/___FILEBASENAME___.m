3e4b2f2b7f088e816de6e071092fb90b23100e6c
IPa <ipapamagic@gmail.com>
2014-01-16 23:18:30 +0800

    Add BaseViewController Template

---
+++ IPa BaseViewController Object.xctemplate/UIViewController/___FILEBASENAME___.m
@@ -0,0 +1,44 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+#pragma mark - UIStoryboard
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if([segue.identifier isEqualToString:<#Embeded Segue Name#>]) {
+        self.___VARIABLE_currentViewController___ = segue.destinationViewController;
+    }
+}
+@end
